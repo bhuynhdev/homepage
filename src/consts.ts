e74cad656a3a59e1ada45f5cc18ab6b8eb42cc31
bhuynhdev <baoh.inbox@gmail.com>
2025-11-16 18:25:16 -0500

    Add basic About section with description and skill list

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,8 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SKILLS = {
+  Frontend: ['React', 'Svelte', 'Astro', 'Typescript', 'JavaScript', 'TailwindCSS', 'CSS3', 'HTML5'],
+  Backend: ['NodeJS', 'Python', 'Perl', 'Elixir', 'Phoenix', 'MySQL', 'PostgreSQL', 'MongoDB'],
+  Others: ['Linux', 'Git', 'Cloudflare'],
+};
